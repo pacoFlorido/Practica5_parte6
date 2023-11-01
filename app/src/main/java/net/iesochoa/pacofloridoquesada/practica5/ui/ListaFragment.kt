0db15401bc3e93094be50908ef9d86889ec278fc
Paco Florido <franciscoflorido0@gmail.com>
2023-11-01 13:30:27 +0100

    Inicio

---
+++ app/src/main/java/net/iesochoa/pacofloridoquesada/practica5/ui/ListaFragment.kt
@@ -0,0 +1,45 @@
+package net.iesochoa.pacofloridoquesada.practica5.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import net.iesochoa.pacofloridoquesada.practica5.R
+import net.iesochoa.pacofloridoquesada.practica5.databinding.FragmentListaBinding
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class ListaFragment : Fragment() {
+
+    private var _binding: FragmentListaBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentListaBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonFirst.setOnClickListener {
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
