71a22aed98344cbe8e0f558a95af2d80f7dca396
Paco Florido <franciscoflorido0@gmail.com>
2023-11-01 13:31:02 +0100

    Punto 11

--- app/src/main/java/net/iesochoa/pacofloridoquesada/practica5/ui/TareaFragment.kt
+++ app/src/main/java/net/iesochoa/pacofloridoquesada/practica5/ui/TareaFragment.kt
@@ -27,7 +27,6 @@ class TareaFragment : Fragment() {
 
         _binding = FragmentTareaBinding.inflate(inflater, container, false)
         return binding.root
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
