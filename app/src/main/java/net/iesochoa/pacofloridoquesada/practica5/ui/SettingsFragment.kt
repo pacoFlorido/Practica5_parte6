80fa93ece4dd67df4164150afdd9b1023cb3dc00
Paco Florido <franciscoflorido0@gmail.com>
2024-01-17 18:58:53 +0100

    Inicio parte 7

--- app/src/main/java/net/iesochoa/pacofloridoquesada/practica5/ui/SettingsFragment.kt
+++ app/src/main/java/net/iesochoa/pacofloridoquesada/practica5/ui/SettingsFragment.kt
@@ -1,12 +1,27 @@
 package net.iesochoa.pacofloridoquesada.practica5.ui
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import net.iesochoa.pacofloridoquesada.practica5.R
 
 class SettingsFragment : PreferenceFragmentCompat() {
-
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
+        // Buscamos la preferencia
+        val buildVersion: Preference? = findPreference("buildVersion")
+        // Definimos la accion para la preferencia
+        buildVersion?.setOnPreferenceClickListener {
+            startActivity(
+                Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse("https://portal.edu.gva.es/03013224/")
+                )
+            )
+            // Hay que devolver booleano para indicar si se acepta el cambio o no
+            false
+        }
     }
 }
