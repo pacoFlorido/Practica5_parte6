6b946d5b2c8e52b6138dbd2d14f33c460cf5bdfe
Paco Florido <franciscoflorido0@gmail.com>
2024-01-17 20:18:46 +0100

    Fin permisos

--- app/src/main/java/net/iesochoa/pacofloridoquesada/practica5/ui/TareaFragment.kt
+++ app/src/main/java/net/iesochoa/pacofloridoquesada/practica5/ui/TareaFragment.kt
@@ -1,7 +1,20 @@
 package net.iesochoa.pacofloridoquesada.practica5.ui
 
+import android.Manifest
+import android.app.Activity
+import android.app.AlertDialog
+import android.content.ContentValues
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.graphics.Color
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -9,7 +22,11 @@ import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.SeekBar
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.net.toUri
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
@@ -19,6 +36,12 @@ import net.iesochoa.pacofloridoquesada.practica5.R
 import net.iesochoa.pacofloridoquesada.practica5.databinding.FragmentTareaBinding
 import net.iesochoa.pacofloridoquesada.practica5.model.Tarea
 import net.iesochoa.pacofloridoquesada.practica5.viewmodel.AppViewModel
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.OutputStream
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -33,6 +56,117 @@ class TareaFragment : Fragment() {
     val args: TareaFragmentArgs by navArgs()
     private val viewModel: AppViewModel by activityViewModels()
     val esNuevo by lazy { args.tarea == null } // Si la tarea no esta en la lista es porque es nueva
+    // Uri foto de la tarea
+    var uriFoto = ""
+    private val PERMISOS_REQUERIDOS=when {
+        //no se la causa pero en el emulador no solicita este permiso en la versión 34
+        // Build.VERSION.SDK_INT >= 34 ->
+        //Manifest.permission.READ_MEDIA_VISUAL_USER_SELECTED
+        Build.VERSION.SDK_INT >= 33 -> Manifest.permission.READ_MEDIA_IMAGES
+        else -> Manifest.permission.READ_EXTERNAL_STORAGE
+    }
+
+    /**
+     * Función que comprueba los permisos.
+     */
+    fun permisosAceptados() = ContextCompat.checkSelfPermission(
+        requireContext(),
+        PERMISOS_REQUERIDOS
+    ) == PackageManager.PERMISSION_GRANTED
+
+    // Petición de foto de la galería Versión <33
+    private val solicitudFotoGalleryMenorV33 = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            //uri de la foto elegida
+            val uri = result.data?.data
+            val uriCopia = saveBitmapImage(loadFromUri(uri)!!)
+            //mostramos la foto
+            binding.ivFotoTarea.setImageURI(uriCopia)
+            //guardamos la uri
+            uriFoto = uriCopia?.toString() ?: ""
+        }
+    }
+
+    // Petición de foto de la galería version >=33
+    private val solicitudFotoGalleryV33 = registerForActivityResult(
+        ActivityResultContracts.PickVisualMedia()
+    ) { uri ->
+        if (uri != null) {
+            //uri de la foto elegida
+            // val uri = result.data?.data
+            val uriCopia = saveBitmapImage(loadFromUri(uri)!!)
+            //mostramos la foto
+            binding.ivFotoTarea.setImageURI(uriCopia)
+            //guardamos la uri
+            uriFoto = uriCopia?.toString() ?: ""
+        }
+    }
+
+    /**
+     * Método que busca la foto mediante la URI de la misma
+     */
+    private fun buscarFoto() {
+        if (Build.VERSION.SDK_INT >= 33)
+            solicitudFotoGalleryV33.launch(PickVisualMediaRequest(
+                ActivityResultContracts.PickVisualMedia.ImageOnly))
+        else {
+            val intent = Intent(Intent.ACTION_PICK,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            intent.type = "image/*"
+            solicitudFotoGalleryMenorV33.launch(intent)
+        }
+    }
+
+    /**
+     * Método que muestra al usuario el mensaje para informar sobre los permisos
+     * que tiene que aceptar para que funcione la asignación de la foto a una tarea.
+     */
+    fun explicarPermisos() {
+        AlertDialog.Builder(requireContext())
+            .setTitle(android.R.string.dialog_alert_title)
+            .setMessage(getString(R.string.es_necesario_el_permiso_de_lectura_de_fichero_para_mostrar_una_foto_desea_aceptar_los_permisos))
+        //acción si pulsa si
+        .setPositiveButton(android.R.string.ok) { v, _ ->
+            //Solicitamos los permisos de nuevo
+            solicitudPermisosLauncher.launch(PERMISOS_REQUERIDOS)
+            //cerramos el dialogo
+            v.dismiss()
+        }
+            //accion si pulsa no
+            .setNegativeButton(android.R.string.cancel) { v, _ -> v.dismiss() }
+            .setCancelable(false)
+            .create()
+            .show()
+    }
+
+    private val solicitudPermisosLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                // Permission has been granted.
+                buscarFoto()
+            } else {
+                // Permission request was denied.
+                explicarPermisos()
+            }
+        }
+
+    fun iniciaIvBuscarFoto() {
+        binding.ivBuscarFoto.setOnClickListener() {
+            when {
+                //La versión 34 el usuario puede dar permiso a fotos individualmente
+                //el sistema gestiona los permisos
+                Build.VERSION.SDK_INT >= 34->buscarFoto()
+                //para otras versiones si tenemos los permisos
+                permisosAceptados() -> buscarFoto()
+                //no tenemos los permisos : los solicitamos
+                else -> solicitudPermisosLauncher.launch(PERMISOS_REQUERIDOS)
+            }
+        }
+    }
 
     /**
      * Iniciamos la tarea que ya existe
@@ -53,6 +187,8 @@ class TareaFragment : Fragment() {
         binding.tietTecnico.setText(tarea.tecnico)
         binding.etDescripcion.setText(tarea.descripcion)
         (requireActivity() as AppCompatActivity).supportActionBar?.title = "Tarea ${tarea.id}"
+        if (!tarea.fotoUri.isNullOrEmpty())
+            binding.ivFotoTarea.setImageURI(tarea.fotoUri.toUri())
     }
 
     private fun iniciaTecnico() {
@@ -65,7 +201,6 @@ class TareaFragment : Fragment() {
         //lo asignamos
         binding.tietTecnico.setText(tecnico)
     }
-
 
     /**
      * Muestra el mensaje de error
@@ -109,9 +244,9 @@ class TareaFragment : Fragment() {
         val descripcion = binding.etDescripcion.text.toString()
         //creamos la tarea: si es nueva, generamos un id, en otro caso le asignamos su id
         val tarea = if (esNuevo)
-            Tarea(categoria, prioridad, pagado, estado, horas, valoracion, tecnico, descripcion, "")
+            Tarea(categoria, prioridad, pagado, estado, horas, valoracion, tecnico, descripcion, uriFoto)
         else
-            Tarea(args.tarea!!.id, categoria, prioridad, pagado, estado, horas, valoracion, tecnico, descripcion,"")
+            Tarea(args.tarea!!.id, categoria, prioridad, pagado, estado, horas, valoracion, tecnico, descripcion, uriFoto)
         //guardamos la tarea desde el viewmodel
         viewModel.addTarea(tarea)
         //salimos de editarFragment
@@ -286,6 +421,100 @@ class TareaFragment : Fragment() {
         }
     }
 
+    private val TAG = "Practica5"
+    private val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+    private fun saveBitmapImage(bitmap: Bitmap): Uri? {
+        val timestamp = SimpleDateFormat(FILENAME_FORMAT, Locale.US)
+            .format(System.currentTimeMillis())
+        var uri: Uri? = null
+        //Tell the media scanner about the new file so that it is immediately available to the user.
+        val values = ContentValues()
+        values.put(MediaStore.Images.Media.MIME_TYPE, "image/png")
+        values.put(MediaStore.Images.Media.DATE_ADDED, timestamp)
+        //mayor o igual a version 29
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            values.put(MediaStore.Images.Media.DATE_TAKEN, timestamp)
+            values.put(
+                MediaStore.Images.Media.RELATIVE_PATH,
+                "Pictures/" + getString(R.string.app_name)
+            )
+            values.put(MediaStore.Images.Media.IS_PENDING, true)
+            uri = requireContext().contentResolver.insert(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                values
+            )
+            if (uri != null) {
+                try {
+                    val outputStream = requireContext().contentResolver.openOutputStream(uri)
+                    if (outputStream != null) {
+                        try {
+                            bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream)
+                            outputStream.close()
+                        } catch (e: Exception) {
+                            Log.e(TAG, "saveBitmapImage: ", e)
+                        }
+                    }
+                    values.put(MediaStore.Images.Media.IS_PENDING, false)
+                    requireContext().contentResolver.update(uri, values, null, null)
+                    // Toast.makeText(requireContext(), "Saved...", Toast.LENGTH_SHORT).show()
+                } catch (e: Exception) {
+                    Log.e(TAG, "saveBitmapImage: ", e)
+                }
+            }
+        } else {//no me funciona bien en versiones inferiores a la 29(Android 10)
+            val imageFileFolder = File(
+                Environment.getExternalStorageDirectory()
+                    .toString() + '/' + getString(R.string.app_name)
+            )
+            if (!imageFileFolder.exists()) {
+                imageFileFolder.mkdirs()
+            }
+            val mImageName = "$timestamp.png"
+            val imageFile = File(imageFileFolder, mImageName)
+            try {
+                val outputStream: OutputStream = FileOutputStream(imageFile)
+                try {
+                    bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream)
+                    outputStream.close()
+                } catch (e: Exception) {
+                    Log.e(TAG, "saveBitmapImage: ", e)
+                }
+                values.put(MediaStore.Images.Media.DATA, imageFile.absolutePath)
+                requireContext().contentResolver.insert(
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                    values
+                )
+                uri = imageFile.toUri()
+                // Toast.makeText(requireContext(), "Saved...", Toast.LENGTH_SHORT).show()
+            } catch (e: Exception) {
+                Log.e(TAG, "saveBitmapImage: ", e)
+            }
+        }
+        return uri
+    }
+
+    fun loadFromUri(photoUri: Uri?): Bitmap? {
+        var image: Bitmap? = null
+        try {
+            // check version of Android on device
+            image = if (Build.VERSION.SDK_INT > 27) {
+                // on newer versions of Android, use the new decodeBitmap method
+                val source = ImageDecoder.createSource(
+                    requireContext().contentResolver,
+                    photoUri!!
+                )
+                ImageDecoder.decodeBitmap(source)
+            } else {
+                // support older versions of Android by using getBitmap
+                MediaStore.Images.Media.getBitmap(requireContext().contentResolver,
+                    photoUri)
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+        return image
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
@@ -308,6 +537,7 @@ class TareaFragment : Fragment() {
         iniciarRgEstado()
         iniciarSbHorasTrabajadas()
         iniciarFabGuardar()
+        iniciaIvBuscarFoto()
 
         if (esNuevo) {
             // Si la tarea es nueva pondremos de titulo "Nueva tarea" y si no pondremos "Tarea" + el id de esa tarea
